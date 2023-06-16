3b84e57b3dae0f11dc3b5fc7a82c1bdf4f29483f
Vitória Silva <silva.lopes.vitoria@gmail.com>
2023-06-15 23:17:41 -0300

    Ajustes gerais de funcionamento e padronização
    Ajustes gerais de funcionamento da navegação e responsividade das páginas e padronização do arquivo da home para o mesmo que foi usado no arquivo de alterar senha.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+  $(".nav-link").click(function () {
+    $("#menu-mobile-content").slideToggle("slow");
+  });
   $("#menu-mobile").click(function () {
     $("#menu-mobile-content").slideToggle("slow");
   });
