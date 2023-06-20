12bcb563ce0aa530cc66f66abe4f7d5605a23270
Vitória Silva <silva.lopes.vitoria@gmail.com>
2023-06-20 20:53:49 -0300

    Removendo toggle .nav-link

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,4 @@
 $(document).ready(function () {
-  $(".nav-link").click(function () {
-    $("#menu-mobile-content").slideToggle("slow");
-  });
   $("#menu-mobile").click(function () {
     $("#menu-mobile-content").slideToggle("slow");
   });
