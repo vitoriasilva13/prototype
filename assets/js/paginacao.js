bbcfdf5025c6784533d21b38e1a629471f6299ea
Vitória Silva <silva.lopes.vitoria@gmail.com>
2023-06-20 21:51:18 -0300

    Criada a página de perfil
    Criada a página e a paginação de perfil do usuário

--- assets/js/paginacao.js
+++ assets/js/paginacao.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
   $("#v-pills-home").load("paginacao/home.html");
   $("#v-pills-settings").load("paginacao/alterar-senha.html");
+  $("#v-pills-perfil").load("paginacao/perfil.html");
 });
