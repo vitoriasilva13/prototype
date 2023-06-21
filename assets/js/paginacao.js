eee85bd1565594a080fcc8ffc029eab565590774
Vitória Silva <silva.lopes.vitoria@gmail.com>
2023-06-20 23:25:36 -0300

    Ajustes gerais e criação da página de projetos
    - Criando a página que mostrará os projetos cadastrados pelo usuário no sistema.
    - Adicionada a paginação para a página de projetos cadastrados
    - Alterada a palavra 'Home' para 'Início' seguindo o padrão do protótipo
    - Ajustes nos estilos e página de perfil

--- assets/js/paginacao.js
+++ assets/js/paginacao.js
@@ -2,4 +2,5 @@ $(document).ready(function () {
   $("#v-pills-home").load("paginacao/home.html");
   $("#v-pills-settings").load("paginacao/alterar-senha.html");
   $("#v-pills-perfil").load("paginacao/perfil.html");
+  $("#v-pills-projetos").load("paginacao/projetos.html");
 });
