df381482927c1bb80ccf1b1f064877c960a29582
Vitória Silva <silva.lopes.vitoria@gmail.com>
2023-06-22 09:57:32 -0300

    Criada a tela de prompt

--- assets/js/paginacao.js
+++ assets/js/paginacao.js
@@ -3,4 +3,6 @@ $(document).ready(function () {
   $("#v-pills-settings").load("paginacao/alterar-senha.html");
   $("#v-pills-perfil").load("paginacao/perfil.html");
   $("#v-pills-projetos").load("paginacao/projetos.html");
+  $("#v-pills-temp1").load("paginacao/projeto-form.html");
+  $("#v-pills-temp2").load("paginacao/prompt.html");
 });
